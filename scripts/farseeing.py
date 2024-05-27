337cdefef9cb8c8d54009d4e2b9479e04a033c4e
Timilehin Aderinola <timiderinola@gmail.com>
2024-05-27 14:46:12 +0000

    load data done

---
+++ scripts/farseeing.py
@@ -0,0 +1,205 @@
+import os
+import mat73
+# import utils
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.signal import resample
+
+def get_description(sensor_location=None):
+    cols = ['Randomnumber', 'Setting', 'Sensor_location', 'Sample_rate_Hz',
+        'Pre_fall_activity_reported', 'fall_direction_reported']
+    meta = pd.read_excel(r'data/FARSEEING/description.xlsx', engine='openpyxl', usecols=cols)
+    if sensor_location == 'L5':
+        meta = meta[meta['Sensor_location']=='L5']
+    if sensor_location == 'Thigh':
+        meta = meta[meta['Sensor_location']=='Thigh']
+  
+    return meta
+
+def load_signal_files(show_plot=False, save_plot=False):
+    meta = get_description()
+    signal_files = os.listdir(r'data/FARSEEING/signals')
+    falls_dict = {}
+    for sf in signal_files:
+        p, ID = sf[2:].split('-')[:2]
+        if p in falls_dict:
+            falls_dict[p].append(sf)
+        else:
+            falls_dict[p] = [sf]
+
+    if show_plot or save_plot:
+        fig, axs = plt.subplots(2,3, figsize=(12,6), dpi=150, sharey='row')
+        for sf in signal_files:
+            fall_id = '-'.join(sf.split("_")[1].split("-")[:2])
+            row = meta[meta['Randomnumber']==fall_id]
+            freq = row['Sample_rate_Hz'].item()
+            signal = mat73.loadmat(f'data/FARSEEING/signals/{sf}')
+            time = signal['tmp'][:,0]
+            accel = signal['tmp'][:,2:5]/9.8
+            accel_norm = np.clip(accel, -2, 2)
+            labels = ['x','y','z']
+            for i, sig in enumerate(accel_norm.T):
+                if freq == 20: # resample to make freq=100Hz
+                    sig = resample(sig, 120000)
+                if row['Sensor_location'].item() == 'L5':
+                    axs[0,i].plot(sig)
+                else:
+                    axs[1,i].plot(sig)
+                axs[0,i].set_title(f'Acceleration {labels[i]}')
+        for ax in axs.flatten():
+            ymin, ymax = ax.get_ylim()
+            ax.scatter(60000, ymin, label='fall')
+        fig.supylabel(r'Accel (g)')
+        fig.supxlabel('Time in seconds')
+        axs[0,0].set_ylabel('Sensor on L5')
+        axs[1,0].set_ylabel('Sensor on Thigh')
+        fig.tight_layout()
+        plt.legend()
+        if save_plot:
+            plt.savefig('/figs/signal_files.pdf')
+        if show_plot:
+            plt.show()
+        
+    return signal_files, falls_dict, meta
+
+def train_test_subjects_split(prefall=1, fall=1, postfall=25.5, thresh=1.4, adl_samples=None, visualize=True):
+    signal_files, falls_dict, meta = load_signal_files()
+    subjects = list(falls_dict.keys())
+    # Have 65 subjects for training, 27 for testing
+    test_set = ['74827807', '74905787', '75240038', '76573505', '79232001', '79336438', '79666043', '79761947', '80061866', '87486959', '88051353', '89647122', '91923026', '91943076', '92097726', '92680167', '93169462', '93807530', '95030446', '95205003', '95253031', '96201346', '96856291', '97085274', '97097674', '97946301', '98843998']
+    train_set = list(set(subjects) - set(test_set))
+    X_train = []; y_train = [];
+    X_test = []; y_test = [];
+    pip = 27
+
+    # fig, axs = plt.subplots(1,3, figsize=(12,4), dpi=150, sharey=True)
+    for sf in signal_files:
+        if sf == "F_00002186-05-2013-11-23-18-25-04.mat":
+            continue
+        fall_id = '-'.join(sf.split("_")[1].split("-")[:2])
+        row = meta[meta['Randomnumber']==fall_id]
+        if row['Sensor_location'].item() != 'L5':
+            continue
+        subject = fall_id.split("-")[0]
+        test = subject in test_set
+        freq = row['Sample_rate_Hz'].item()
+        signal = mat73.loadmat(f'data/FARSEEING/signals/{sf}')
+        time = signal['tmp'][:,0]
+        accel = signal['tmp'][:,2:5]/9.8
+        accel_magnitude = magnitude(np.clip(accel, -2, 2))
+        fall_indicator = signal['tmp'][:,11]
+        fall_point = np.where(fall_indicator!=0)[0][0]
+        # window_size = prefall + fall + postfall 
+        # end_with_padding = len(accel_magnitude) - int(freq * window_size)
+        # extract the fall <prefall> sec before, and <fall+postfall> secs after
+        before = int(fall_point-(freq*prefall))
+        after = int(fall_point+(freq*(fall+postfall)))
+        fall_signal = accel_magnitude[before:after]
+        prefall_signal = accel_magnitude[:before]
+        # Segment fall_signal
+        X_train, X_test, y_train, y_test = get_windows(
+            X_train, X_test, y_train, y_test, fall_signal, freq,
+            target=1, test=test,
+            prefall=prefall, fall=fall, postfall=postfall)
+        # Segment prefall_signal
+        X_train, X_test, y_train, y_test = get_windows(
+            X_train, X_test, y_train, y_test, prefall_signal, freq,
+            target=0, thresh=thresh, test=test,
+            prefall=prefall, fall=fall, postfall=postfall)
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
+    if adl_samples is not None:
+        X_train, y_train = sample_adls(X_train, y_train, adl_samples)
+    print(f"Train set: X: {X_train.shape}, y: {y_train.shape}\
+    ([ADLs, Falls])", np.bincount(y_train))
+    print(f"Test set: X: {X_test.shape}, y: {y_test.shape}\
+    ([ADLs, Falls])", np.bincount(y_test))
+    if visualize:
+        visualize_falls_adls(X_train, y_train)
+    return X_train, y_train, X_test, y_test
+
+def sample_adls(X_train, y_train, adl_samples):
+    # Group falls and ADLs together to sample from ADLs
+    X_train_y_train = np.concatenate([X_train, y_train.reshape(-1,1)], axis=1)
+    X_train_falls = X_train_y_train[X_train_y_train[:,-1]==1]
+    X_train_ADLs = X_train_y_train[X_train_y_train[:,-1]==0]
+    np.random.seed(5)
+    rng = np.random.default_rng()
+    rng.shuffle(X_train_ADLs)
+    # Select <adl_samples> ADLs
+    X_train_ADLs_with_labels = X_train_ADLs[:adl_samples]
+    X_train_rejoined = np.concatenate([X_train_ADLs_with_labels,
+                                    X_train_falls], axis=0)
+    rng.shuffle(X_train_rejoined) # shuffle again
+    # recover X_train and y_train
+    X_train = X_train_rejoined[:,:-1]
+    y_train = X_train_rejoined[:,-1].astype(int)
+    return X_train, y_train
+
+def visualize_falls_adls(X_train, y_train, save=True):
+    fig, axs = plt.subplots(1, 2, figsize=(12, 4), dpi=150,
+                        sharey=True, layout='tight')
+    fallers = y_train.astype(bool)
+    falls = X_train[fallers]
+    adls = X_train[fallers == False]
+    axs[0].plot(adls.T, color='lightblue')
+    axs[0].plot(adls.mean(axis=0), color='blue', label='mean sample')
+    axs[0].set_title('ADL samples')
+    axs[0].set_ylabel('Accel magnitude (g)')
+    
+    axs[1].plot(falls.T, color='lightblue')
+    axs[1].plot(falls.mean(axis=0), color='blue', label='mean sample')
+    axs[1].set_title('Fall samples')
+    
+    fig.suptitle("Mean ADLs and fall samples in the training set")
+    axs[1].legend()
+    if save:
+        plt.savefig('figs/adls_vs_falls.pdf', bbox_inches='tight')
+    plt.show()
+
+def expand_for_ts(X_train, X_test):
+    X_train = np.array(X_train)[:, np.newaxis, :]
+    X_test = np.array(X_test)[:, np.newaxis, :]
+    return X_train, X_test
+
+def get_windows(X_train, X_test, y_train, y_test,
+    ts, freq, target, thresh=1.08, step=1, test=False, pip=False,
+    prefall=1, fall=1, postfall=25.5):
+    # Main fall_window = 1 sec, prefall window = 1 sec
+    # postfall window = 1 sec, recovery window = 24.5 secs
+    total_duration = prefall + fall + postfall
+    sample_window_size = int(freq*total_duration)
+    required_length = int(freq*(total_duration))
+    freq_100_length = int(100*(total_duration))
+    # resample to match signals of 100Hz if necessary
+    resample_to_100Hz = freq_100_length != required_length
+    for j in range(0, len(ts), freq*step):
+        potential_window = ts[j:j+sample_window_size]
+        if len(potential_window) < required_length:
+            break
+        main_window = potential_window[freq:2*freq]
+        if len(main_window) == freq*step:
+            if max(main_window) >= thresh:
+                selected_window = potential_window
+                if resample_to_100Hz:
+                    selected_window = resample(selected_window, freq_100_length)
+                if test:
+                    X_test.append(selected_window)
+                    y_test.append(target)
+                else:
+                    X_train.append(selected_window)
+                    y_train.append(target)
+    return X_train, X_test, y_train, y_test
+
+def magnitude(arr):
+    x, y, z = arr.T
+    magnitude = np.sqrt(x**2 + y**2 + z**2)
+    magnitude -= min(magnitude)
+    return magnitude
+
+# if __name__ == '__main__':
+#     X_train, y_train, X_test, y_test = train_test_subjects_split()
---
+++ scripts/utils.py
@@ -0,0 +1,248 @@
+# import pywt
+import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
+from scipy.signal import resample
+from sklearn.metrics import f1_score
+import time, timeit
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import ConfusionMatrixDisplay
+
+from sklearn.preprocessing import LabelEncoder
+from torch.utils import data
+
+
+# apply threshold to positive probabilities to create labels
+def to_labels(pos_probs, threshold):
+    return (pos_probs >= threshold).astype('int')
+
+def predict_eval(model, X_in=None, y_in=None, starttime=None, adapt_threshold=False):
+    target_names = ['ADL', 'Fall']
+    model_name, clf = model
+    has_proba = hasattr(clf, 'predict_proba')
+    if X_in is not None:
+        X_train, X_test = X_in
+        y_train, y_test = y_in
+    print("classifier:", model_name)
+    if starttime is None:
+        starttime = timeit.default_timer()
+    clf.fit(X_train, y_train)
+    if has_proba:
+        probs = clf.predict_proba(X_test)[:, 1]
+        train_probs = clf.predict_proba(X_train)[:, 1]
+    else:
+        if adapt_threshold:
+            adapt_threshold = False
+            print("Setting adapt_threshold=False since chosen classifier has no predict_proba() method")
+    if adapt_threshold:
+        thresholds = np.arange(0, 1, 0.001)
+        # evaluate each threshold
+        scores = [f1_score(y_train, to_labels(train_probs, t)) for t in thresholds]
+        # get best threshold
+        ix = np.argmax(scores)
+        print('Threshold=%.3f, F-Score=%.5f' % (thresholds[ix], scores[ix]))
+        y_pred = to_labels(probs, thresholds[ix])
+    else:
+        y_pred = clf.predict(X_test)
+    print("Time to train + test (sec):", timeit.default_timer() - starttime)
+    if has_proba:
+        print(f'AUC: {np.round(roc_auc_score(y_test, probs), 2)}')
+    else:
+        print("Skipping AUC since chosen classifier has no predict_proba() method")
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.grid(False)
+    plt.show()
+    print(classification_report(y_test, y_pred, target_names=target_names))
+
+def point_line_distance(line, p):
+    """Calculates the distance between a point and a line defined by two points.
+
+    Args:
+    line => p1, p2
+        p1: First point on the line (tuple or list).
+        p2: Second point on the line (tuple or list).
+    p: The point to find the distance from (tuple or list).
+
+    Returns:
+    The distance between the point and the line.
+    """
+    p1, p2 = line
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p.T
+    # Distance from (p3) to the line defined by (p1) and (p2)
+    numerator = abs((y2 - y1) * x3 - (x2 - x1) * y3 + x2 * y1 - y2 * x1)
+    denominator = sqrt((y2 - y1)**2 + (x2 - x1)**2)
+    return numerator / denominator
+
+def get_pips(y, k=None, visualize=True, **kwargs):
+    # set k to half the length of y by default
+    if k is None:
+        k = max(3, int(len(y)/2))
+    assert k > 2, "k must be greater than 2"
+    default_kwargs = {
+        'xlabel': '', 'ylabel': '',
+        'figsize': (10,3),
+        'dpi': 150,
+        'title': 'Perceptually Important Points'
+    }
+    kwargs = {**default_kwargs, **kwargs}
+    x = np.arange(0, len(y)).reshape(-1,1)
+    y_arr = np.array(y).reshape(-1,1)
+    points = np.concatenate([x, y_arr], axis=1)
+    line = (0, len(y)-1) # initial line
+    pips = find_pips(points, line, k)
+    pips.sort()
+    if visualize:
+        visualize_pips(y, pips, **kwargs)
+    return y[pips]
+
+def visualize_pips(y, pips, **kwargs):
+    plt.figure(figsize=kwargs['figsize'], dpi=kwargs['dpi'])
+    plt.plot(y, label='Time Series')
+    plt.plot(pips, y[pips], 'x', label='PIPs')
+    plt.xlabel(kwargs['xlabel'])
+    plt.ylabel(kwargs['ylabel'])
+    plt.title(kwargs['title'])
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def find_pips(points, line, k, pips_list=None):
+    if pips_list is None: 
+        pips_list = [0, len(points)-1] # initial pips
+    # select split point
+    window = points[line[0]:line[1]]
+    if len(pips_list) <= k and len(window) > 2:
+        line_coords = (points[line[0]], points[line[1]])
+        distances_arr = point_line_distance(line_coords, window)
+        new_pip = np.argmax(distances_arr)
+        pips_list.append(new_pip)
+        # make new lines
+        left_line = (line[0], new_pip)
+        right_line = (new_pip, line[1])
+        left_is_longer = len(left_line) >= len(right_line)
+        new_line = left_line if left_is_longer else right_line
+        return find_pips(points, new_line, k, pips_list)
+    else:
+        return pips_list
+
+def get_windows(X_train, X_test, y_train, y_test,
+    ts, freq, target, thresh=1.08, step=1, test=False, pip=False,
+    prefall=1, fall=1, postfall=25.5):
+    # Main fall_window = 1 sec, prefall window = 1 sec
+    # postfall window = 1 sec, recovery window = 24.5 secs
+    total_duration = prefall + fall + postfall
+    sample_window_size = int(freq*total_duration)
+    required_length = int(freq*(total_duration))
+    freq_100_length = int(100*(total_duration))
+    # resample to match signals of 100Hz if necessary
+    resample_to_100Hz = freq_100_length != required_length
+    end = len(ts) - int(freq * total_duration)
+    count = 0
+    for j in range(0, len(ts), freq*step):
+        # potential_window = ts[j-int(freq*prefall):j+int(postfall*freq)]
+        potential_window = ts[j:j+sample_window_size]
+        if len(potential_window) < required_length:
+            break
+        main_window = potential_window[freq:2*freq]
+        if len(main_window) == freq*step:
+            if max(main_window) >= thresh:
+                selected_window = potential_window
+                count+=1
+                # if len(selected_window) < required_length:
+                    # excluded.append((selected_window, "selected_window", "not long enough"))
+                    # continue
+                if resample_to_100Hz:
+                    selected_window = resample(selected_window, freq_100_length)
+                if pip:
+                    selected_window = get_pips(selected_window,
+                        k=pip, visualize=False)
+                    selected_window = resample(selected_window, pip)
+                if test:
+                    X_test.append(selected_window)
+                    y_test.append(target)
+                else:
+                    X_train.append(selected_window)
+                    y_train.append(target)
+                # n_windows += 1
+            # else:
+                # excluded.append((main_window, "main_window", "max < 1.4"))
+        # else:
+            # excluded.append((main_window, "main_window", "wrong length"))
+    print(f'target: {target}, count: {count}')
+    return X_train, X_test, y_train, y_test
+
+def magnitude(arr):
+    x, y, z = arr.T
+    magnitude = np.sqrt(x**2 + y**2 + z**2)
+    magnitude -= min(magnitude)
+    return magnitude
+
+############# from Davide #################
+def gen_cube(instance):
+    result = []
+    for i in range(len(instance)):
+        result.append([instance[(i+j)%len(instance)] for j in range(len(instance))])
+    return result
+
+def one_hot_encoding(train_labels,test_labels):
+    enc = LabelEncoder()
+    y_train = enc.fit_transform(train_labels)
+    y_test = enc.transform(test_labels)
+
+    return y_train,y_test,enc
+
+
+
+def pre_fature_normalization(X_train,X_test):
+    eps = 1e-6
+    f_mean = X_train.mean(axis=0, keepdims=True)
+    f_std = X_train.std(axis=0, keepdims=True) + eps  # epsilon to avoid dividing by 0
+    X_train_tfm2 = (X_train - f_mean) / f_std
+    X_test_tfm2 = (X_test - f_mean) / f_std
+    return  X_train_tfm2,X_test_tfm2
+
+
+def plot_dCAM( instance, dcam, nb_dim, idx ):
+    plt.figure(figsize=(20,5))
+    plt.title('multivariate data series')
+    for i in range(len(instance)):
+        plt.subplot(len(instance),1,1+i)
+        plt.plot(instance[i])
+        plt.xlim(0,len(instance[i]))
+        plt.yticks([0],["Dim {}".format(i)])
+
+    plt.figure(figsize=(20,5))
+    #plt.title('dCAM')
+    plt.imshow(dcam,aspect='auto',interpolation=None)
+    plt.yticks(list(range(nb_dim)), ["Dim {}".format(i) for i in range(nb_dim)])
+    plt.savefig("tmp/"+str(idx)+".png")
+    #plt.colorbar(img)
+
+
+def minMax_normalization(X, epsillon=0.0000000001):
+    #X  = np.abs(X)
+    zeros = np.zeros(shape=X.shape)
+    X = np.maximum(X,zeros)
+    X = (X - X.min() ) / ( (X.max() - X.min())  + epsillon)
+    return X
+
+class MyDataset(data.Dataset):
+
+    def __init__(self,X,y):
+        super().__init__()
+        self.X = X
+        self.y = y
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+############# from Davide #################

5859a2e8a09b5cb3ef72cf9309994e751f4f7244
Timilehin Aderinola <timiderinola@gmail.com>
2024-05-30 15:17:34 +0000

    started first time series experiment

--- scripts/classifiers.py
+++ scripts/classifiers.py
@@ -7,7 +7,6 @@ sns.set_style("ticks")
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 from scripts import farseeing
-# from torch import nn
 
 from sklearn.linear_model import RidgeClassifierCV, RidgeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -25,8 +24,6 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler, Normalizer
 from sklearn.metrics import ConfusionMatrixDisplay
 
-# from scripts.hydra import Hydra, SparseScaler
-
 from aeon.classification.deep_learning import InceptionTimeClassifier
 from aeon.classification.deep_learning import FCNClassifier
 from aeon.classification.distance_based import KNeighborsTimeSeriesClassifier
@@ -35,10 +32,6 @@ from aeon.classification.interval_based import DrCIFClassifier
 from aeon.classification.interval_based import QUANTClassifier
 from aeon.classification.convolution_based import RocketClassifier, HydraClassifier
 from aeon.classification.convolution_based import MultiRocketHydraClassifier
-# from aeon.transformations.collection.shapelet_based import RandomShapeletTransform
-
-# from sktime.transformations.panel.rocket import Rocket, MiniRocket, MultiRocket
-# from sktime.transformations.panel.catch22 import Catch22
 
 
 def run_tabular_models(X_train, y_train, X_test, y_test, window_size=2750):
@@ -101,84 +94,7 @@ def plot_metrics(df, x='model', pivot='f1-score', compare='metrics', **kwargs):
         sns.despine()
     else:
         df.plot(kind='bar', x='model', y=compare)
-    plt.legend(loc=9, ncol=3, bbox_to_anchor=(0.5,1.3), title=compare)
-# def run_hydra(X_train, y_train, X_test, y_test, final_clf, **kwargs):
-#     X_train, X_test = farseeing.expand_for_ts(X_train, X_test)
-#     X_train_torch = torch.tensor(X_train, dtype=torch.float32)
-#     X_test_torch = torch.tensor(X_test, dtype=torch.float32)
-
-#     start = timeit.default_timer()
-#     hydra_transform = Hydra(X_train_torch.shape[-1], seed=0)
-#     X_training_transform = hydra_transform(X_train_torch)
-#     X_test_transform = hydra_transform(X_test_torch)
-#     scaler = SparseScaler()
-#     X_training_transform = scaler.fit_transform(X_training_transform)
-#     X_test_transform = scaler.transform(X_test_transform)
-#     metrics = predict_eval(final_clf, kwargs['window_size'],
-#                            X_in=(X_training_transform, X_test_transform),
-#                            y_in=(y_train, y_test), starttime=start)
-#     return metrics
-
-# def add_final_clf(transform):
-#     clfs = {
-#         'RidgeCV': RidgeClassifierCV(alphas = np.logspace(-3, 3, 10)),
-#         'ExtraTrees': ExtraTreesClassifier(
-#             n_estimators=150, max_features=0.1, criterion="entropy", n_jobs=-1, random_state=0)
-#     }
-#     models = []
-#     for clf_name, clf in clfs.items():
-#         models.append((f'{transform}+{clf_name}', clf))
-    
-#     return models
-
-# def run_hydra(X_train, y_train, X_test, y_test, window_size):
-#     X_train, X_test = farseeing.expand_for_ts(X_train, X_test)
-#     X_train_torch = torch.tensor(X_train, dtype=torch.float32)
-#     X_test_torch = torch.tensor(X_test, dtype=torch.float32)
-#     hydra_models = add_final_clf('Hydra')
-    
-#     metrics_df = pd.DataFrame(columns=['model', 'window_size', 'runtime', 'precision', 'recall', 'f1-score'])
-#     for hydra_model in hydra_models:
-#         start = timeit.default_timer()
-#         hydra_transform = Hydra(X_train_torch.shape[-1], seed=40)
-#         X_training_transform = hydra_transform(X_train_torch)
-#         X_test_transform = hydra_transform(X_test_torch)
-
-#         scaler = SparseScaler()
-#         X_training_transform = scaler.fit_transform(X_training_transform)
-#         X_test_transform = scaler.transform(X_test_transform)
-#         metrics = predict_eval(hydra_model, window_size,
-#                            X_in=(X_training_transform, X_test_transform),
-#                            y_in=(y_train, y_test), starttime=start)
-#         this_df = pd.DataFrame(data=metrics)
-#         metrics_df = pd.concat([metrics_df, this_df])    
-#     return metrics_df            
-
-# def run_rocket(X_train, y_train, X_test, y_test, window_size):
-#     rocket_models = add_final_clf('Rocket')
-#     for rocket_model in rocket_models:
-#         start = timeit.default_timer()
-#         rocket = Rocket(random_state=0).fit(X_train)
-#         X_train_transform = rocket.transform(X_train)
-#         X_test_transform = rocket.transform(X_test)
-#         metrics = predict_eval(rocket_model, window_size,
-#                            X_in=(X_train_transform, X_test_transform),
-#                            y_in=(y_train, y_test), starttime=start)
-#         this_df = pd.DataFrame(data=metrics)
-#         metrics_df = pd.concat([metrics_df, this_df])    
-#     return metrics_df
-
-# def run_ts_models(X_train, y_train, X_test, y_test, window_size=2750):
-
-#     # Hydra
-#     metrics_df = run_hydra(X_train, y_train, X_test, y_test, window_size)
-#     # Rocket
-#     rocket_metrics = run_rocket(X_train, y_train, X_test, y_test, window_size)
-#     metrics_df = pd.concat([metrics_df, rocket_metrics], ignore_index=True)
-
-#     return metrics_df
-    
-
+    plt.legend(loc=9, ncol=3, bbox_to_anchor=(0.5,1.3), title=compare) 
 
 def to_labels(pos_probs, threshold):
     # apply threshold to positive probabilities to create labels
